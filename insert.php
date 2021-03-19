dd1c1f3fff0af8791203e12e8e33cf2e9e66c566
DawidSzafranskii <dawid.szaf22@gmail.com>
2021-03-19 12:19:39 +0100

    .

---
+++ insert.php
@@ -0,0 +1,29 @@
+<?php
+
+
+ 
+$servername = "db";
+$username = "root";
+$password = "root";
+$dbname = "tom";
+ 
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("<li>Connection failed: " . $conn->connect_error);
+}else{
+    echo("<li>Connection ok");
+}
+ 
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
+echo ("<br>");
+ 
+echo $sql;
+if ($conn->query($sql) === TRUE) {
+  echo ("<br>"."Pracownik został pomyślnie dodany.");
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+ 
+$conn->close();
+?>
